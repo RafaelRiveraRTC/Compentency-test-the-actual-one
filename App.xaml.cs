c794143d6b6ee5791f65d049c0b45395c02bd6ac
Emiliano <emilianovolk75@gmail.com>
2024-06-03 17:47:43 -0700

    Add project files.

---
+++ App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace Compentency_test_the_actual_one
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
